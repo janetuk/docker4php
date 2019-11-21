b6df31eafadeafc63b3c4368c37b8d3be9a239df
Neil McKett <neil.mckett@jisc.ac.uk>
2019-11-21 09:22:11 +0000

    Added tests directory for syncing.

--- docker.mk
+++ docker.mk
@@ -40,6 +40,10 @@ shell:
 
 fpm:
 	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell  docker ps --filter ancestor=wodby/php:7.2-dev-4.11.5 --format '{{ .ID }}'  ) sh
+
+fpmi:
+	docker exec -i -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell  docker ps --filter ancestor=wodby/php:7.2-dev-4.11.5 --format '{{ .ID }}'  ) sh -c "${CMD}"
+
 logs:
 	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
 
