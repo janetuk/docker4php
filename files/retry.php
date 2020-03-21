fae54b9e6ee7bc58fb3a44174829a311bef12bbe
Neil McLKett <neil.mckett@jisc.ac.uk>
2020-03-21 22:18:54 +0000

    We have to run composer to copy over index.php etc.

--- files/retry.php
+++ files/retry.php
@@ -1,5 +1,6 @@
 <?php
+system ('sudo /root/sync >& /dev/null');
 sleep (1);
-header('Location: http://' . $_SERVER['HTTP_HOST'] . '/' . $_GET['q']); 
+header('Location: http://' . $_SERVER['HTTP_HOST'] . '/' . $_GET['q'] . '?' . $_GET['a']); 
 exit;
 
